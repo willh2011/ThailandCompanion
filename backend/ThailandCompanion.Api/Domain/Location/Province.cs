5b5c1513a9730e449250f3936d578b77738d604b
William Haskins <willh2011@live.com>
2026-06-29 09:00:39 +0700

    Added Province domain model

---
+++ backend/ThailandCompanion.Api/Domain/Location/Province.cs
@@ -0,0 +1,8 @@
+namespace ThailandCompanion.Api.Domain.Location;
+
+public class Province
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
+}
