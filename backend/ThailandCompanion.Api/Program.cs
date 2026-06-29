6f41ea3ea4625faa38fa01aa2c2283d2595e10c7
William Haskins <willh2011@live.com>
2026-06-29 08:35:58 +0700

    Replaced weather endpoint with health check

--- backend/ThailandCompanion.Api/Program.cs
+++ backend/ThailandCompanion.Api/Program.cs
@@ -1,50 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-//Adding to make swagger work
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-//app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-// Should be before endpoints
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/api/health", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return Results.Ok(new
+    {
+        application = "Thailand Companion",
+        version = "0.0.1",
+        status = "Healthy",
+        environment = app.Environment.EnvironmentName,
+        utcTime = DateTime.UtcNow
+    });
 })
-.WithName("GetWeatherForecast");
+.WithName("HealthCheck");
 
 app.Run();
-
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
