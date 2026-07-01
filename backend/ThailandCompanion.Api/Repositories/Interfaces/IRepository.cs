b90e8f3ce1271b1877c6f55243b62f1754e42850
William Haskins <willh2011@live.com>
2026-07-01 10:14:13 +0700

    Added generic repository layer

---
+++ backend/ThailandCompanion.Api/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+using System.Linq.Expressions;
+
+namespace ThailandCompanion.Api.Repositories.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    IQueryable<T> Query();
+
+    Task<List<T>> GetAllAsync();
+
+    Task<T?> GetByIdAsync(int id);
+
+    Task AddAsync(T entity);
+
+    Task AddRangeAsync(IEnumerable<T> entities);
+
+    void Update(T entity);
+
+    void Remove(T entity);
+}
---
+++ backend/ThailandCompanion.Api/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using ThailandCompanion.Api.Data;
+using ThailandCompanion.Api.Repositories.Interfaces;
+
+namespace ThailandCompanion.Api.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public IQueryable<T> Query()
+    {
+        return _dbSet.AsQueryable();
+    }
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public async Task AddRangeAsync(IEnumerable<T> entities)
+    {
+        await _dbSet.AddRangeAsync(entities);
+    }
+
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    public void Remove(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+}
